cbc51becd3c2d3a4ed42ef7c18ffe2c1b71aba52
Artem <moiseev@physics.msu.ru>
2023-07-21 14:31:24 +0300

    Initial Commit

---
+++ BetterRest(100SUI)/BetterRest_100SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest_100SUI_App.swift
+//  BetterRest(100SUI)
+//
+//  Created by Artem on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest_100SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
